30139f43c61dba9a4dd7616b964bba603d3ebaf8
Ryan Vo <ryanvo@seas.upenn.edu>
2017-01-25 20:58:09 -0500

    finish board and game

--- src/Board.java
+++ src/Board.java
@@ -3,31 +3,41 @@
  */
 public class Board {
 
-    private char[][] board;
+    private int[][] board;
     private int marksMade = 0;
 
     public Board(int row, int col) {
-        board = new char[row][col];
+        board = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                board[i][j] = 0;
+            }
+        }
+
     }
 
     public boolean markMyMove(int x, int y) {
-        if (board[x][y] == 'o') {
+        if (board[x][y] == 0 || board[x][y] == -1) {
             return false;
         } else {
-            board[x][y] = 'm';
+            board[x][y] = '1';
             marksMade++;
             return true;
         }
     }
 
     public boolean markOpponentMove(int x, int y) {
-        if (board[x][y] == 'm') {
+        if (board[x][y] == 0 || board[x][y] == 1) {
             return false;
         } else {
-            board[x][y] = 'o';
+            board[x][y] = -1;
             marksMade++;
             return true;
         }
+    }
+
+    public int at(int x, int y) {
+        return board[x][y];
     }
 
     public int rows() {
@@ -39,10 +49,7 @@ public class Board {
     }
 
     public boolean isFull() {
-
         return marksMade == rows() * cols();
-
-
     }
 
 }
--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * @author rtv
  */
@@ -8,27 +10,70 @@ public class TicTacToeGame implements Game {
 
     @Override
     public void init(Player p1, Player p2) {
-
         this.p1 = p1;
         this.p2 = p2;
-
     }
 
     @Override
-    public void play() {
+    public Player play() {
 
         Mark p1turn, p2turn = null;
 
         while (!board.isFull()) {
             p1turn = p1.doTurn(p2turn);
+            if (this.hasWinner())
+                return p1;
+
             p2turn = p2.doTurn(p1turn);
+            if (this.hasWinner())
+                return p2;
         }
 
-
+        return null;
     }
 
     @Override
-    public String getWinner() {
-        return null;
+    public String printResult() {
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board.at(i,j) == 1) {
+                    result.append("X ");
+                } else if (board.at(i, j) == 0) {
+                    result.append("_ ");
+                } else {
+                    result.append("O ");
+                }
+            }
+            result.append("\n");
+        }
+
+        return result.toString();
     }
+
+    @Override
+    public boolean hasWinner() {
+
+        /* Check rows + cols */
+        for (int i = 0; i < board.rows(); i++) {
+            int rowSum = 0;
+            int colSum = 0;
+            for (int j = 0; j < board.cols(); j++) {
+                rowSum += board.at(i, j);
+                colSum += board.at(j, i);
+            }
+            if (Math.abs(rowSum) == 3 || Math.abs(colSum) == 3)
+                return true;
+        }
+
+        /* Check diagonals */
+        int diagSum = board.at(0, 0) + board.at(1, 1) + board.at(2,2);
+        int otherDiagSum = board.at(2, 0) + board.at(1, 1) + board.at(0,
+                2);
+
+        return (Math.abs(diagSum) == 3) || (Math.abs(otherDiagSum) == 3);
+    }
+
 }
