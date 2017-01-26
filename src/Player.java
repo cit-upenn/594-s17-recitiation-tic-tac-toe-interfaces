932b8967dcce0c9c76ebbcaec66365a7fe21d5c1
Ryan Vo <ryanvo@seas.upenn.edu>
2017-01-25 21:04:46 -0500

    clarify player

--- src/Player.java
+++ src/Player.java
@@ -9,7 +9,8 @@ public interface Player {
     public String name();
 
     /**
-     *
+     * Respond to a turn with a Mark indicating the next move.
+     * The initial turn will receive a null Mark object.
      * @param prevMarkMadeByOpponent coordinates of the last mark made
      * @return the next mark to make
      */
