0ab64c06f33fbdf476b49b6c6bd2446cb2ce5cfd
Ryan Vo <ryanvo@seas.upenn.edu>
2017-01-25 20:57:19 -0500

    complete game interface

--- src/Game.java
+++ src/Game.java
@@ -3,10 +3,28 @@
  */
 public interface Game {
 
+    /**
+     * Initializes the game with two players
+     */
     void init(Player p1, Player p2);
 
-    void play();
+    /**
+     * Runs the game and returns the winning player or null if there isn't one
+     */
+    Player play();
 
-    String getWinner();
+    /**
+     * Returns true if there is a winner at the end of the game
+     */
+    boolean hasWinner();
+
+    /**
+     * Print an ASCII representation of the final game state
+     *  e.g. X O O
+     *       O X X
+     *       O X O
+     */
+    String printResult();
+
 
 }
